a511df5ba019727dc1030a2bfa4991ed6f379de3
benny glot <bennyglot@gmail.com>
2020-11-10 15:07:37 +0200

    update project structure: client folder server folder

---
+++ client/build/precache-manifest.c463151a06c3a062ab72566eacc2faff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2bc7aee3961d20b3cd65ace1f7d665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbb87c89cbecded80df",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af33a91602d3266bcc90",
+    "url": "/static/js/2.04dcf979.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.04dcf979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8675f9480540ef1a40",
+    "url": "/static/js/3.b78c35fd.chunk.js"
+  },
+  {
+    "revision": "fcbb87c89cbecded80df",
+    "url": "/static/js/main.4819d1bf.chunk.js"
+  },
+  {
+    "revision": "373337bc902281841664",
+    "url": "/static/js/runtime-main.5163e30a.js"
+  }
+]);
---
+++ client/build/static/js/3.b78c35fd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{103:function(n,a,e){"use strict";e.r(a);var t=e(0),p=e.n(t),r=function(){return p.a.createElement(p.a.Fragment,null,"main panel")};a.default=r}}]);
+//# sourceMappingURL=3.b78c35fd.chunk.js.map
