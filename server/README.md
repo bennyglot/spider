4f94ddeaf4428bb34533da01d9b67c6a2e0c029b
benny glot <bennyglot@gmail.com>
2020-11-12 14:03:18 +0200

    add env file. open connection to mongodb -- need to decorate...base connection.

---
+++ server/README.md
@@ -0,0 +1,6 @@
+Main Project.
+
+will supprt api.
+
+1 make interface with depenedcies.
+2 make login api set mongodb schema
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+console.log(`connectionString; ${require('dotenv').config({debug: true})}`);
+mongoose.connect(connectionString, function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Successfully connected to ${process.env.MONGODB_CONNECTION_STRING}`);
+  }
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
