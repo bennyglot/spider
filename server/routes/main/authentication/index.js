abe4a18918ad14dc690be3bf8722a3e163839046
benny glot <bennyglot@gmail.com>
2020-11-18 16:05:39 +0200

    server add withAuth middleware. add 2 routes: authenticaton check token.

--- server/routes/main/authentication/index.js
+++ server/routes/main/authentication/index.js
@@ -1,23 +1,30 @@
 const AuthenticationRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const AuthenticationController = require('../../../controllers/main/authentication');
+const withAuth = require('../../../middleware/withAuth');
 
 // POST route to register a user
-AuthenticationRouter.post('/', AuthenticationController.register);
+AuthenticationRouter.post('/', withAuth, AuthenticationController.register);
 
 // Retrieve all users
-AuthenticationRouter.get("/", AuthenticationController.findAll);
+AuthenticationRouter.get("/", withAuth, AuthenticationController.findAll);
 
 // Retrieve a single user with id
-AuthenticationRouter.get("/:id", AuthenticationController.findOne);
+AuthenticationRouter.get("/:id", withAuth, AuthenticationController.findOne);
 
 // Update a user with id
-AuthenticationRouter.put("/:id", AuthenticationController.update);
+AuthenticationRouter.put("/:id", withAuth, AuthenticationController.update);
 
 // Delete a user with id
-AuthenticationRouter.delete("/:id", AuthenticationController.delete);
+AuthenticationRouter.delete("/:id", withAuth, AuthenticationController.delete);
 
 // Create a new user
-AuthenticationRouter.delete("/", AuthenticationController.deleteAll);
+AuthenticationRouter.delete("/", withAuth, AuthenticationController.deleteAll);
+
+//autenticate
+AuthenticationRouter.post('/api/authenticate', AuthenticationController.authenticate);
+
+//checktoken
+AuthenticationRouter.get('/checkToken', withAuth, AuthenticationController.checkToken);
 
 module.exports = AuthenticationRouter;
-
